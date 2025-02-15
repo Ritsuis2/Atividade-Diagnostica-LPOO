28bc134c05fdced7cb8bab11b210045e0ebd770e
Raniele Xavier <Ranyxavier5@gmail.com>
2025-02-15 09:20:00 -0300

    A média pode ser ponderada (com pesos) ou simples

--- escola/Nota.java
+++ escola/Nota.java
@@ -15,24 +15,38 @@ class Nota {
         nota1 = null;
         nota2 = null;
         nota3 = null;
+        peso1 = 1.0; // Caso não haja pesos, a média será simples
+        peso2 = 1.0;
+        peso3 = 1.0;
     }
 
     public double calcularMedia() {
         if ((nota1 == null) || (nota2 == null) || (nota3 == null)) {
-            return -1
+            return -1;
         }
-        double media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
-        return media;
+
+        boolean pesosDefinidos = (peso1 != null && peso2 != null && peso3 != null);
+
+        if (pesosDefinidos) {
+            // Média ponderada
+            return (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+        } else {
+            // Média simples
+            return (nota1 + nota2 + nota3) / 3;
+        }
     }
 
     public String getSituacao() {
         double media = calcularMedia();
         if (media >= curso
-         {
+           {
+
+        }
+        ,getMediaRecuperacao()
             
-        },getMediaRecuperacao()
-            
-        ) {
+        
+        
+            ) {
             return "Aprovado";
         }else if (media >=curso.getMediaRecuperacao()) {
             return "Em recuperação";
@@ -40,4 +54,5 @@ class Nota {
             return "Reprovado";
         }
     }
+
 }
