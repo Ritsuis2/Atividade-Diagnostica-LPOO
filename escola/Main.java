9bb6bb45c211fada201c07e8bf65739057da3315
Raniele Xavier <165105347+Ritsuis2@users.noreply.github.com>
2025-02-27 00:37:42 -0300

    Corrigido erro na variável `notaRec` dentro da  Main.java

--- escola/Main.java
+++ escola/Main.java
@@ -12,21 +12,19 @@ public class Main {
         Professor professor2 = new Professor("Rosangela Garcia", "111.111.11-1", "Rua Z, Bairro W", "912345678", 67890);
 
         Estudante estudante1 = new Estudante("Joao Souza", "222.222.22-2", "Rua A, Bairro B", "987654321", "2023001");
-        estudante1.adicionarNota(8.5, 6.0, 9.0, 3.0, 3.0, 4.0);  // Corrigido: Passando Double explícito
-
+        estudante1.adicionarNota(8.5, 6.0, 9.0, 3.0, 3.0, 4.0);  
         Estudante estudante2 = new Estudante("Mirele Oliveira", "333.333.33-3", "Rua C, Bairro D", "998877665",
                 "2023002");
-        estudante2.adicionarNota(7.0, 8.0, 8.5, 4.0, 3.0, 3.0);  // Corrigido: Passando Double explícito
+        estudante2.adicionarNota(7.0, 8.0, 8.5, 4.0, 3.0, 3.0); 
 
         Estudante estudante3 = new Estudante("Ana Costa", "444.444.444-4", "Rua E, Bairro F", "985633211", "5552668");
-        estudante3.adicionarNota(5.0, 4.5, 6.0, 2.0, 3.0, 5.0);  // Corrigido: Passando Double explícito
-
+        estudante3.adicionarNota(5.0, 4.5, 6.0, 2.0, 3.0, 5.0);  
         Estudante estudante4 = new Estudante("Andressa Mota", "555.555.555-5", "Rua G, Bairro H", "998544721",
                 "5586441");
-        estudante4.adicionarNota(10.0, 9.0, 8.5, 5.0, 3.0, 2.0);  // Corrigido: Passando Double explícito
+        estudante4.adicionarNota(10.0, 9.0, 8.5, 5.0, 3.0, 2.0);  
 
         Estudante estudante5 = new Estudante("Analice Silva", "666.666.666-6", "Rua I, Bairro J", "9971655", "2020322");
-        estudante5.adicionarNota(2.0, 3.5, 4.0, 3.0, 3.0, 4.0);  // Corrigido: Passando Double explícito
+        estudante5.adicionarNota(2.0, 3.5, 4.0, 3.0, 3.0, 4.0); 
 
         estudante5.adicionarNotaRecuperacao(6.0);
 
@@ -38,7 +36,7 @@ public class Main {
             System.out.println("2. Ver logs de um estudante");
             System.out.println("0. Sair");
             opcao = scanner.nextInt();
-            scanner.nextLine();  // Consumir a quebra de linha
+            scanner.nextLine();  
 
             switch (opcao) {
                 case 1:
@@ -82,9 +80,11 @@ public class Main {
 
         System.out.println("==== Turma 1 ====");
         turma1.print();
-
+        turma1.emitirListaFinal();
+        
         System.out.println("==== Turma 2 ====");
         turma2.print();
+        turma2.emitirListaFinal(); 
 
     }
 }
